3e51fff878a2a1d8c18dec8067841edbd643cf13
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-04-21 10:00:00 -0400

    Added delete button for saved stories

--- mad-react/src/components/Saved.js
+++ mad-react/src/components/Saved.js
@@ -9,11 +9,25 @@ function Saved() {
         .then(resp => resp.json())
         .then(savedStories => setSavedStories(savedStories))
     }, [])
+
+    function handleDelete(story) {
+        fetch(`http://localhost:3000/saved-stories/${story.id}`,{
+            method: "DELETE",
+            header: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => resp.json())
+        .then(setSavedStories(savedStories.filter(savedStory => {
+            return savedStory.id !== story.id
+        })))
+    }
     
     const renderSaved = savedStories.map(story => {
         return (
             <li key={story.id}>
                 <Link to={`/savedstory/${story.id}`}>{story.title}</Link>
+                <button onClick={() => handleDelete(story)}>Delete</button>
             </li>
         )
     })
