731b1ae84b8c04c074fb9f1feff9213006e16af6
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-04-20 21:37:38 -0400

    Story is merged and displays after form submit

--- mad-react/src/components/SubmittedStory.js
+++ mad-react/src/components/SubmittedStory.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"
 
 
 function SubmittedStory({ inputs }) {
-    console.log(inputs)
+    const [story, setStory] = useState({})
+    const [prompt, setPrompt] = useState([])
+    const params = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/stories/${params.id}`)
+        .then(resp => resp.json())
+        .then(story => {
+            setStory(story)
+            return setPrompt(story.value)
+        })
+    }, [params.id])
+
+    if (!story) return <h2>Loading...</h2>
+
+    prompt.pop()
+
+    const joinedArr=[]
+
+    for (let i = 0; i <= inputs.length; i++) {
+        joinedArr.push(prompt.slice(i))
+        joinedArr.push(inputs.slice(i))
+    } 
+
+    const mappedArr = joinedArr.map(array => {
+        return array[0]
+    })
+
+    const joinedStory = mappedArr.join('')
+    
     return (
-        <h1>Hi</h1>
+        <div className="finished-story">
+            <h1>{story.title}</h1>
+            <h4>{joinedStory}</h4>
+
+        </div>
     )
 }
 
